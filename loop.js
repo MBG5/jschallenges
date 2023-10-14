423917e72f111b699bdd766401161f65fd35bf05
= <=mohamed74181@outlook.com>
2023-10-14 13:20:21 +0100

    second commit

---
+++ loop.js
@@ -0,0 +1,46 @@
+for (let i = 0; i < 10; i++) {
+   console.log("mohamed")
+    }
+
+
+while (condition) {
+    
+}
+
+
+
+
+for (let i = 0;i<10;i++){
+console.log("mohamed")}
+
+
+
+let count = 0
+while(count<10){
+    console.log("mohamed")
+    i++
+}
+
+let today = "satuday"
+
+if(today=="saturday")
+{console.log("come to the coding school")
+} else if(today == "sunday")
+{console.log("sleep")}
+else if (today == "monday")
+{console.log("go to school")}
+else {console.log("do whatever you like")}
+
+
+switch (today){
+    case"saturday":
+        console.log("codinding time...")
+        break;
+    case"sunday":
+        console.log("sleeping time...")
+        break;
+    case"monday":
+         console.log("School time...")
+         break
+    
+}
